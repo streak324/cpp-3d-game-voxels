7fda5b3a636055accf7667a1725eb5b7ee8b8645
Andrew Sepulveda <streak324@gmail.com>
2022-10-28 22:26:36 -0700

    create vullkan instance

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## dependencies
 - Visual Studio 2022
 - [GLFW](https://www.glfw.org/) 3.3.8
-	- download windows binaries zip, and unzip into C:\GLFW
-- [Vulkan SDK](https://vulkan.lunarg.com/) 1.3.211.0
-	- install in C:\VulkanSDK
+	- download windows binaries zip, and unzip into `C:\GLFW`
+- [Vulkan SDK](https://vulkan.lunarg.com/) 1.3.231.1
+	- install in `C:\VulkanSDK`
--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <windows.h>
+#include <malloc.h>
 
-//#include <vulkan/vulkan.h>
+#include <vulkan/vulkan.h>
 #include <GLFW/glfw3.h>
 
 typedef int8_t i8;
@@ -19,39 +20,95 @@ typedef uint16_t u16;
 typedef float f32;
 typedef double f64;
 
-#define GIGABYTE(a) (u64) a * 1000*1000*1000
+inline void Assert(u8 b) {
+	if (!b) {
+		*((char*) 0) = 0;
+	}
+}
 
 int main(void)
 {
-	u8 * memory_pool = (u8*) malloc(GIGABYTE(1));
-    GLFWwindow* window;
+	/* Initialize the library */
+	if (!glfwInit())
+	{
+		return -1;
+	}
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(1280, 720, "CPP Voxels!!", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
+	VkApplicationInfo appInfo = {};
+	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	appInfo.pApplicationName = "Hello Triangle";
+	appInfo.applicationVersion = VK_API_VERSION_1_3;
+	appInfo.pEngineName = "No Engine";
+	appInfo.engineVersion = VK_API_VERSION_1_3;
+	appInfo.apiVersion = VK_API_VERSION_1_3;
+
+	VkInstanceCreateInfo createInfo{};
+	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	createInfo.pApplicationInfo = &appInfo;
+
+	u32 glfwExtensionCount = 0;
+	const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+	u32 requiredExtensionCount = 1;
+	const u32 requiredExtensionCapacity = 1024;
+	const char * requiredExtensions [requiredExtensionCapacity] = {
+		VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME,
+	};
+	Assert(glfwExtensionCount + requiredExtensionCount < requiredExtensionCapacity);
+	for (i32 i=0; i < glfwExtensionCount; i++) {
+		requiredExtensions[i+requiredExtensionCount] = glfwExtensions[i];
+	}
+	requiredExtensionCount += glfwExtensionCount;
+
+	createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+
+	createInfo.enabledExtensionCount = glfwExtensionCount;
+	createInfo.ppEnabledExtensionNames = glfwExtensions;
+	createInfo.enabledLayerCount = 0;
+
+	VkInstance instance;
+
+
+	u32 extensionCount = 0;
+	vkEnumerateInstanceExtensionProperties(NULL, &extensionCount, NULL);
+
+	VkExtensionProperties * extensions = (VkExtensionProperties*) _malloca(extensionCount * sizeof(VkExtensionProperties));
+	vkEnumerateInstanceExtensionProperties(NULL, &extensionCount, extensions);
+
+	for (i32 i=0; i < extensionCount; i++) {
+		printf("extension: %s\n", extensions[i]);
+	}
+
+	if(vkCreateInstance(&createInfo, NULL, &instance) != VK_SUCCESS) {
+		fprintf(stderr, "failed to create instance!");
+		exit(1);
+	}
+
+	/* Create a windowed mode window and its OpenGL context */
+	GLFWwindow* window = glfwCreateWindow(1280, 720, "CPP Voxels!!", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
 		printf("unable to create the window");
-        return -1;
-    }
+		return -1;
+	}
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
 
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
 
-    glfwTerminate();
-    return 0;
+	glfwTerminate();
+	return 0;
 }
