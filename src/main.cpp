689f9d51dc5f52ac53128c9ec3c8387badc8b572
Andrew Sepulveda <streak324@gmail.com>
2022-10-29 10:52:54 -0700

    check that device supports required extensions

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include <windows.h>
 #include <malloc.h>
+#include <string.h>
 
 #include <vulkan/vulkan.h>
 #include <GLFW/glfw3.h>
@@ -31,7 +32,8 @@ int main(void)
 	/* Initialize the library */
 	if (!glfwInit())
 	{
-		return -1;
+		printf("unable to initialize glfw!\n");
+		return 1;
 	}
 
 	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
@@ -45,17 +47,14 @@ int main(void)
 	appInfo.engineVersion = VK_API_VERSION_1_3;
 	appInfo.apiVersion = VK_API_VERSION_1_3;
 
-	VkInstanceCreateInfo createInfo{};
-	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-	createInfo.pApplicationInfo = &appInfo;
-
 	u32 glfwExtensionCount = 0;
 	const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
 
-	u32 requiredExtensionCount = 1;
+	u32 requiredExtensionCount = 0;
 	const u32 requiredExtensionCapacity = 1024;
 	const char * requiredExtensions [requiredExtensionCapacity] = {
-		VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME,
+		//mac os thing
+		//VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME,
 	};
 	Assert(glfwExtensionCount + requiredExtensionCount < requiredExtensionCapacity);
 	for (i32 i=0; i < glfwExtensionCount; i++) {
@@ -63,28 +62,50 @@ int main(void)
 	}
 	requiredExtensionCount += glfwExtensionCount;
 
+	u32 extensionCount = 0;
+	vkEnumerateInstanceExtensionProperties(NULL, &extensionCount, NULL);
+
+	VkExtensionProperties * extensions = (VkExtensionProperties*) _malloca(extensionCount * sizeof(VkExtensionProperties));
+	const char* *extensionNames = (const char**) _malloca(extensionCount * sizeof(char*));
+	vkEnumerateInstanceExtensionProperties(NULL, &extensionCount, extensions);
+
+	for (i32 i=0; i < extensionCount; i++) {
+		printf("extension: %s\n", extensions[i].extensionName);
+		extensionNames[i] = extensions[i].extensionName;
+	}
+
+
+	for (i32 i=0; i < requiredExtensionCount; i++) {
+		printf("required extension %s\n", requiredExtensions[i]);
+		u8 found = 0;
+		for (i32 j=0; j < extensionCount; j++) {
+			if (strcmp(requiredExtensions[i], extensionNames[j]) == 0) {
+				found = 1;
+				break;
+			}
+		}
+		if (!found) {
+			printf("extension %s is not supported in this device!\n", requiredExtensions[i]);
+			return 1;
+		}
+	}
+
+	VkInstanceCreateInfo createInfo{};
+	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	createInfo.pApplicationInfo = &appInfo;
+
 	createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
 
-	createInfo.enabledExtensionCount = glfwExtensionCount;
-	createInfo.ppEnabledExtensionNames = glfwExtensions;
+	createInfo.enabledExtensionCount = extensionCount;
+	createInfo.ppEnabledExtensionNames = extensionNames;
 	createInfo.enabledLayerCount = 0;
 
 	VkInstance instance;
 
 
-	u32 extensionCount = 0;
-	vkEnumerateInstanceExtensionProperties(NULL, &extensionCount, NULL);
-
-	VkExtensionProperties * extensions = (VkExtensionProperties*) _malloca(extensionCount * sizeof(VkExtensionProperties));
-	vkEnumerateInstanceExtensionProperties(NULL, &extensionCount, extensions);
-
-	for (i32 i=0; i < extensionCount; i++) {
-		printf("extension: %s\n", extensions[i]);
-	}
-
 	if(vkCreateInstance(&createInfo, NULL, &instance) != VK_SUCCESS) {
 		fprintf(stderr, "failed to create instance!");
-		exit(1);
+		return 1;
 	}
 
 	/* Create a windowed mode window and its OpenGL context */
@@ -93,7 +114,7 @@ int main(void)
 	{
 		glfwTerminate();
 		printf("unable to create the window");
-		return -1;
+		return 1;
 	}
 
 	/* Make the window's context current */
